7a3ee531a9b55f2f0cc55278e89d3a3a74e93840
Tung Hoang <cayxungvenduong@gmail.com>
2019-07-10 23:47:24 +0700

    remove don't need

--- my_json.py
+++ my_json.py
@@ -1,9 +1,8 @@
 import json
 
-listClient = []
 with open('data.json') as f:
     data = json.load(f)
 
-tk1 = next(filter(lambda x: x["token"] == "token2", data), None)
+var1 = next(filter(lambda x: x["token"] == "token2", data), None)
 
-print(tk1)
+print(var1)
